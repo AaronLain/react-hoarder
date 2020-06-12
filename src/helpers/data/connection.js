710fd9eca2ddc543450be56312906be9b4f04786
Aaron Lain <aaron.j.lain@gmail.com>
2020-06-12 13:03:11 -0500

    connection no test yet

--- src/helpers/data/connection.js
+++ src/helpers/data/connection.js
@@ -1,1 +1,11 @@
-import firebase from 'firebas'
+import firebase from 'firebase';
+
+import firebaseConfig from '../apiKeys.json';
+
+const firebaseApp = () => {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig.firebaseKeys);
+  }
+};
+
+export default firebaseApp;
