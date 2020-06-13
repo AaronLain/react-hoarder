73664ac9139af92a95df9e54d1d918c81ac5573b
Aaron Lain <aaron.j.lain@gmail.com>
2020-06-13 12:08:51 -0500

    delete works on card

--- src/components/shared/ItemCard/ItemCard.js
+++ src/components/shared/ItemCard/ItemCard.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+import itemShape from '../../../helpers/propz/itemShape';
 import './ItemCard.scss';
 
 class ItemCard extends React.Component {
+  static propTypes = {
+    item: itemShape.itemShape,
+    removeItem: PropTypes.func.isRequired,
+  }
+
   render() {
-    const { item } = this.props;
+    const { item, removeItem } = this.props;
     const singleLink = `/items/${item.id}`;
     return (
       <div className="ScatCard col-3">
@@ -15,7 +22,7 @@ class ItemCard extends React.Component {
             <h5 className="card-title">{item.itemName}</h5>
             <Link className="btn btn-info" to={singleLink}>Single</Link>
             {/* <Link className="btn btn-warning" to={editLink}><i className="fas fa-pencil-alt"></i></Link> */}
-            {/* <button className="btn btn-danger" onClick={() => removeScat(scat.id)}><i className="fas fa-trash-alt"></i></button> */}
+            <button className="btn btn-danger" onClick={() => removeItem(item.id)}>Delete!</button>
             <p className="card-text">{item.itemDescription}</p>
           </div>
         </div>
